779207028b2abcc68d58f847937cb42041b2ca55
Roberto1313 <67878026+robertomarroquin1313@users.noreply.github.com>
2023-07-08 15:56:55 -0600

    Primer Commit

---
+++ src/main/java/com/mycompany/gatosapp/GatosApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gatosapp;
+
+/**
+ *
+ * @author Roberto Marroquín
+ */
+public class GatosApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
