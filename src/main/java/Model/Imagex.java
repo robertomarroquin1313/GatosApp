c401dfba0491094ca796ce6738df68c74f7d6289
Roberto1313 <67878026+robertomarroquin1313@users.noreply.github.com>
2023-07-11 23:27:11 -0600

    ImagexClass

---
+++ src/main/java/Model/Imagex.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Roberto Marroquín
+ */
+public class Imagex {
+      String id;
+    String url;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
