027b80dfa7757b9489740255dc5e56a29bac25f2
Roberto1313 <67878026+robertomarroquin1313@users.noreply.github.com>
2023-07-11 23:27:55 -0600

    modificacion de funciones

--- src/main/java/Services/GatosService.java
+++ src/main/java/Services/GatosService.java
@@ -5,6 +5,7 @@
 package Services;
 
 import Model.Gatos;
+import Model.GatosFav;
 import com.google.gson.Gson;
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
@@ -23,94 +24,184 @@ import javax.swing.JOptionPane;
  * @author Roberto Marroquín
  */
 public class GatosService {
-    public static void verGatitos() throws IOException{
+    public static void verGatitos() throws IOException {
         //1. Traer los datos de la api
         OkHttpClient client = new OkHttpClient();
-       
-        Request request = new Request.Builder().url("https://api.thecatapi.com/v1/images/search").get().build();
+
+        Request request = new Request.Builder()
+                .url("https://api.thecatapi.com/v1/images/search")
+                .get()
+                .build();
         Response response = client.newCall(request).execute();
-        
-        String jsonResponse = response.body().string();
-        
+
+
+        String elJson = response.body().string();
+
         //Cortar corchetes
-        jsonResponse =jsonResponse.substring(1,jsonResponse.length());
-        jsonResponse =jsonResponse.substring(0,jsonResponse.length()-1);
-        
+        elJson = elJson.substring(1, elJson.length());
+        elJson = elJson.substring(0, elJson.length() - 1);
+
         //crea objeto de tipo Gson
-        Gson gson = new Gson();
-        Gatos gatitos = gson.fromJson(jsonResponse,Gatos.class);
-        
+        Gatos gatitos = new Gson().fromJson(elJson, Gatos.class);
+
         //redimencionar en caso de necesitar
         Image image = null;
-        
-        try{
+
+        try {
             URL url = new URL(gatitos.getUrl());
             image = ImageIO.read(url);
-            
-            
+
+
             ImageIcon fondoGato = new ImageIcon(image);
-            
-            if(fondoGato.getIconWidth()>800){
+
+            if (fondoGato.getIconWidth() > 800) {
                 //redimencionar
                 Image fondo = fondoGato.getImage();
-                Image modifica = fondo.getScaledInstance(800, 600, java.awt.Image.SCALE_SMOOTH);
-                fondoGato= new ImageIcon(modifica);
-                
+                Image modifica = fondo.getScaledInstance(800, 600, Image.SCALE_SMOOTH);
+                fondoGato = new ImageIcon(modifica);
+
             }
-            
+
             String menu = "Opciones: \n"
                     + "1. Ver otra Imagen\n"
                     + "2. Marcar como favorito\n"
                     + "3. Volver al menu\n";
-            
-            String[] btns = {
-                "Ver otra imagen","Favoritos","Volver"
-            };
-            String idGato = gatitos.getIdGato();
-            String op = (String) JOptionPane.showInputDialog(null,menu,idGato,JOptionPane.INFORMATION_MESSAGE,
-                    fondoGato,btns,btns[0]);
+
+            String[] btns = {"Ver otra imagen", "Favoritos", "Volver"};
+            String idGato = gatitos.getId();
+
+            String op = (String) JOptionPane.showInputDialog(null, menu, idGato, JOptionPane.INFORMATION_MESSAGE,
+                    fondoGato, btns, btns[0]);
+
             int selec = -1;
-            
-            for(int i = 0; i<btns.length;i++){
-                if(op.equals(btns[i])){
-                    selec= i;
+
+            for (int i = 0; i < btns.length; i++) {
+                if (op.equals(btns[i])) {
+                    selec = i;
                 }
             }
-            
-            switch(selec){
+
+            switch (selec) {
                 case 0:
                     verGatitos();
                     break;
-                    
+
                 case 1:
                     favoritoGato(gatitos);
                     break;
-                    
+
                 default:
                     break;
-                    
+
             }
-            
-        }catch(IOException err){
-            System.out.println("Error: "+err);
+
+        } catch (IOException err) {
+            System.out.println("Error: " + err);
         }
-        
+
     }
-    
-    public static void favoritoGato(Gatos gato){
-        try{
+
+    public static void favoritoGato(Gatos gato) {
+        try {
             OkHttpClient client = new OkHttpClient();
             MediaType mediaType = MediaType.parse("application/json");
-            RequestBody body = RequestBody.create(mediaType, "{\r\n\"image_id\":\""+gato.getIdGato()+"\"\r\n}");
+            RequestBody body = RequestBody.create(mediaType, "{\n\t\"image_id\":\"" + gato.getId() + "\"\n}");
             Request request = new Request.Builder()
-            .url("https://api.thecatapi.com/v1/favourites")
-            .method("POST", body)
-            .addHeader("Content-Type", "application/json")
-            .addHeader("x-api-key", gato.getApiKey())
-            .build();
+                    .url("https://api.thecatapi.com/v1/favourites")
+                    .post(body)
+                    .addHeader("Content-Type", "application/json")
+                    .addHeader("x-api-key", gato.getApiKey())
+                    .build();
             Response response = client.newCall(request).execute();
-        }catch(IOException err){
-            System.out.println("errpr: "+err);
+        } catch (IOException err) {
+            System.out.println("errpr: " + err);
         }
+    }
+
+    public static void verFavorito(String apiKey) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder().url("https://api.thecatapi.com/v1/images/search").get()
+                .addHeader("Content-Type", "application/json")
+                .addHeader("x-api-key", apiKey).build();
+
+        Response response = client.newCall(request).execute();
+
+        String jsonResponse = response.body().string();
+        //Crear objeto gson
+        Gson gson = new Gson();
+        GatosFav[] gatosArray = gson.fromJson(jsonResponse, GatosFav[].class);
+
+        if (gatosArray.length > 0) {
+            int min = 1;
+            int max = gatosArray.length;
+
+            int aleatorio = (int) (Math.random() * ((max - min) - 1)) + min;
+
+            int indice = aleatorio - 1;
+            GatosFav gatoFav = gatosArray[indice];
+
+            //redimencionar en caso de necesitar
+            Image image = null;
+
+            try {
+                URL url = new URL(gatoFav.image.getUrl());
+                image = ImageIO.read(url);
+
+
+                ImageIcon fondoGato = new ImageIcon(image);
+
+                if (fondoGato.getIconWidth() > 800) {
+                    //redimencionar
+                    Image fondo = fondoGato.getImage();
+                    Image modifica = fondo.getScaledInstance(800, 600, Image.SCALE_SMOOTH);
+                    fondoGato = new ImageIcon(modifica);
+
+                }
+
+                String menu = "Opciones: \n"
+                        + "1. Ver otra Imagen\n"
+                        + "2. Eliminar favorito\n"
+                        + "3. Volver al menu\n";
+
+                String[] btns = {"Ver otra imagen", "Borrar favoritos", "Volver"};
+                String idGato = gatoFav.getId();
+
+                String op = (String) JOptionPane.showInputDialog(null, menu, idGato, JOptionPane.INFORMATION_MESSAGE,
+                        fondoGato, btns, btns[0]);
+
+                int selec = -1;
+
+                for (int i = 0; i < btns.length; i++) {
+                    if (op.equals(btns[i])) {
+                        selec = i;
+                    }
+                }
+
+                switch (selec) {
+                    case 0:
+                        verGatitos();
+                        break;
+
+                    case 1:
+                        favoritoGato(gatoFav);
+                        break;
+
+                    default:
+                        break;
+
+                }
+
+            }catch (IOException err) {
+                System.out.println("Error: " + err);
+            }
+
+        }
+
+
+    }
+
+    public static void favoritoGato(GatosFav gatosFav) {
+
     }
 }
