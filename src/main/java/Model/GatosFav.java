4efa0761bd403ee0b2d192b30589cf142ba97073
Roberto1313 <67878026+robertomarroquin1313@users.noreply.github.com>
2023-07-11 23:26:51 -0600

    Creacion de gatosFav Class

---
+++ src/main/java/Model/GatosFav.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Roberto Marroquín
+ */
+public class GatosFav {
+     String id;
+    String image_id;
+    String apiKey = "live_DWTmVl6GnwqPQo9rUqRhAofUVY0chPxQROrWjlPN7RvXbuDiUkMvjZwyVxNbmYQ6";
+
+    public Imagex image;
+
+    public Imagex getImage() {
+        return image;
+    }
+
+    public void setImage(Imagex image) {
+        this.image = image;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImage_id() {
+        return image_id;
+    }
+
+    public void setImage_id(String image_id) {
+        this.image_id = image_id;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+}
