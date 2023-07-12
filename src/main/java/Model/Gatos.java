9f0335a1c46974b033a44adeb36238e6a1ae4097
Roberto1313 <67878026+robertomarroquin1313@users.noreply.github.com>
2023-07-11 18:05:25 -0600

    Modelo de Gatos

---
+++ src/main/java/Model/Gatos.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Roberto Marroquín
+ */
+public class Gatos {
+    String idGato;
+    String url;
+    String apiKey = "live_DWTmVl6GnwqPQo9rUqRhAofUVY0chPxQROrWjlPN7RvXbuDiUkMvjZwyVxNbmYQ6";
+    String image;
+
+    public String getIdGato() {
+        return idGato;
+    }
+
+    public void setIdGato(String idGato) {
+        this.idGato = idGato;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+    
+    
+}
